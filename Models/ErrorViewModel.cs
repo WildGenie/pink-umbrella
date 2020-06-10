a94ead307d86755451401fccb0710ac02a02f193
Viveret Steele <viveret.amant@gmail.com>
2020-06-09 22:05:22 -0700

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace seattle.git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
